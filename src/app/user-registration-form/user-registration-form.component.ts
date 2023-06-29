fe3753b38a2d3cf69aeb662f894d552224c5ffb7
Wlad <wlad_diaz@yahoo.com>
2023-06-29 15:52:41 -0400

    fixing issues

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-
-// This import is used to close the dialog on success
-import { MatDialogRef } from '@angular/material/dialog';
-
-// This import brings in the API calls we created in 6.2
 import { FetchApiDataService } from '../fetch-api-data.service';
-
-// This import is used to display notifications back to the user
+import { MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 
@@ -27,7 +21,6 @@ export class UserRegistrationFormComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  // This is the function responsible for sending the form inputs to the backend
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
       // Logic for a successful user registration goes here! (To be implemented)
